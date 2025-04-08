ddca1165aa4137223941cc3206f1a74618fba350
Atlus501 <helloworld4846@gmail.com>
2025-04-07 20:43:14 -0700

    test

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,17 @@
+//this is the main entry point for the backend
+
 const express = require("express");
 const db = require("./models");
 const app = express();
 
+app.use(express.json());
 
+const userRouter = require('./routers/Users');
+app.use("/users", userRouter);
 
-app.listen(3001, () =>{
-console.log("server running on port 3001");
+//this is the function that makes the models in the database
+db.sequelize.sync().then( () =>{
+    app.listen(3001, () =>{
+    console.log("server running on port 3001");
+    });
 });
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,13 +2,17 @@ module.exports = (sequelize, DataTypes) =>{
     const Users = sequelize.define("Users", {
         username:{
             type: DataTypes.STRING,
+            unique: true,
             allowNull: false,
         },
 
         password:{
             type: DataTypes.STRING,
             allowNull: false,
-        }
-    }) //the first parameter creates a table with the name, the second parameter is an object that will define the table’s fields
+        },
+    }); 
+    
+    return Users;
+    //the first parameter creates a table with the name, the second parameter is an object that will define the table’s fields
     }
     
